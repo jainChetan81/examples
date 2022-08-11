3820dbc546d605ed8a81d33078a1aee5a65c5146
jainChetanUnolo <chetan@unolo.com>
2022-08-11 14:56:04 +0530

    completed till refactoring

--- pages/tilting_cards.tsx
+++ pages/tilting_cards.tsx
@@ -5,20 +5,20 @@ const TiltingCards = () => {
 		<Layout title="Tilting Cards with CSS">
 			<section className="tilting_cards-container">
 				<div className="tilting_cards-wrapper">
-          <div className="mouse_position-tracker"></div>
-          <div className="mouse_position-tracker"></div>
-          <div className="mouse_position-tracker"></div>
-          <div className="mouse_position-tracker"></div>
-          <div className="mouse_position-tracker"></div>
-          <div className="mouse_position-tracker"></div>
-          <div className="mouse_position-tracker"></div>
-          <div className="mouse_position-tracker"></div>
-          <div className="mouse_position-tracker"></div>
-          <div className="tilting_card-content">
-            <h1>Tilting Card</h1>
-            <p>CSS Only</p>
-          </div>
-        </div>
+					<div className="mouse_position-tracker"></div>
+					<div className="mouse_position-tracker"></div>
+					<div className="mouse_position-tracker"></div>
+					<div className="mouse_position-tracker"></div>
+					<div className="mouse_position-tracker"></div>
+					<div className="mouse_position-tracker"></div>
+					<div className="mouse_position-tracker"></div>
+					<div className="mouse_position-tracker"></div>
+					<div className="mouse_position-tracker"></div>
+					<div className="tilting_card-content">
+						<h1>Tilting Card</h1>
+						<p>CSS Only</p>
+					</div>
+				</div>
 			</section>
 		</Layout>
 	);
