6ba40ec056d5db4382a05dbcfe22399f259fe686
jain_chetan_spinny <chetan.jain@spinny.com>
2023-05-30 16:03:01 +0530

    small change

--- src/hooks/useTraceUpdate.tsx
+++ src/hooks/useTraceUpdate.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 
-function useTraceUpdate<T extends Record<string, any>>(props: T) {
+function useTraceUpdate<T extends Record<string, any>>(props: T, name: string) {
 	const prev = useRef<T>(props);
 	useEffect(() => {
 		const changedProps = Object.entries(props).reduce((ps: Record<string, any>, [k, v]) => {
@@ -10,9 +10,9 @@ function useTraceUpdate<T extends Record<string, any>>(props: T) {
 			return ps;
 		}, {});
 		if (Object.keys(changedProps).length > 0) {
-			console.log("Changed props:", changedProps);
+			console.log(`${name} Changed props:`, changedProps);
 		}
 		prev.current = props;
-	}, [props]);
+	}, [props, name]);
 }
 export default useTraceUpdate;
