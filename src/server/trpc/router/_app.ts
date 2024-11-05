84d77369ab65a94127ba2acbf66fd545ffa3c70f
jain_chetan_spinny <chetan.jain@spinny.com>
2024-11-05 20:19:19 +0530

    updated repo

--- src/server/trpc/router/_app.ts
+++
@@ -1,9 +0,0 @@
-import { router } from "../trpc";
-import { formsRouter } from "./formsRouter";
-
-export const appRouter = router({
-	forms: formsRouter,
-});
-
-// export type definition of API
-export type AppRouter = typeof appRouter;
